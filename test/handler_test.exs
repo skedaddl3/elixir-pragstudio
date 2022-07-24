6f8c398a2afd81cdaf70202edeebd3b7fb84923b
skedaddl3 <hexisdylan23@gmail.com>
2022-07-24 23:04:37 +0800

    external library

--- test/handler_test.exs
+++ test/handler_test.exs
@@ -36,7 +36,7 @@ defmodule HandlerTest do
     expected_response = """
     HTTP/1.1 200 OK\r
     Content-Type: text/html\r
-    Content-Length: 355\r
+    Content-Length: 377\r
     \r
     <h1>All The Bears!</h1>
 
@@ -54,7 +54,7 @@ defmodule HandlerTest do
     </ul>
     """
 
-    # assert remove_whitespace(response) == remove_whitespace(expected_response)
+    assert remove_whitespace(response) == remove_whitespace(expected_response)
     # temporary fix to tests
   end
 
@@ -137,7 +137,7 @@ defmodule HandlerTest do
     expected_response = """
     HTTP/1.1 200 OK\r
     Content-Type: text/html\r
-    Content-Length: 320\r
+    Content-Length: 324\r
     \r
     <h1>Clark's Wildthings Refuge</h1>
     <blockquote>
@@ -148,7 +148,7 @@ defmodule HandlerTest do
     </blockquote>
     """
 
-    # assert remove_whitespace(response) == remove_whitespace(expected_response)
+    assert remove_whitespace(response) == remove_whitespace(expected_response)
     # temporary fix to tests
   end
 
@@ -175,36 +175,36 @@ defmodule HandlerTest do
            """
   end
 
-  # test "GET /api/bears" do
-  #   request = """
-  #   GET /api/bears HTTP/1.1\r
-  #   Host: example.com\r
-  #   User-Agent: ExampleBrowser/1.0\r
-  #   Accept: */*\r
-  #   \r
-  #   """
+  test "GET /api/bears" do
+    request = """
+    GET /api/bears HTTP/1.1\r
+    Host: example.com\r
+    User-Agent: ExampleBrowser/1.0\r
+    Accept: */*\r
+    \r
+    """
 
-  #   response = handle(request)
+    response = handle(request)
 
-  #   expected_response = """
-  #   HTTP/1.1 200 OK
-  #   Content-Type: application/json
-  #   Content-Length: 605
+    expected_response = """
+    HTTP/1.1 200 OK
+    Content-Type: application/json
+    Content-Length: 605
 
-  #   [{"type":"Brown","name":"Teddy","id":1,"hibernating":true},
-  #    {"type":"Black","name":"Smokey","id":2,"hibernating":false},
-  #    {"type":"Brown","name":"Paddington","id":3,"hibernating":false},
-  #    {"type":"Grizzly","name":"Scarface","id":4,"hibernating":true},
-  #    {"type":"Polar","name":"Snow","id":5,"hibernating":false},
-  #    {"type":"Grizzly","name":"Brutus","id":6,"hibernating":false},
-  #    {"type":"Black","name":"Rosie","id":7,"hibernating":true},
-  #    {"type":"Panda","name":"Roscoe","id":8,"hibernating":false},
-  #    {"type":"Polar","name":"Iceman","id":9,"hibernating":true},
-  #    {"type":"Grizzly","name":"Kenai","id":10,"hibernating":false}]
-  #   """
+    [{"type":"Brown","name":"Teddy","id":1,"hibernating":true},
+     {"type":"Black","name":"Smokey","id":2,"hibernating":false},
+     {"type":"Brown","name":"Paddington","id":3,"hibernating":false},
+     {"type":"Grizzly","name":"Scarface","id":4,"hibernating":true},
+     {"type":"Polar","name":"Snow","id":5,"hibernating":false},
+     {"type":"Grizzly","name":"Brutus","id":6,"hibernating":false},
+     {"type":"Black","name":"Rosie","id":7,"hibernating":true},
+     {"type":"Panda","name":"Roscoe","id":8,"hibernating":false},
+     {"type":"Polar","name":"Iceman","id":9,"hibernating":true},
+     {"type":"Grizzly","name":"Kenai","id":10,"hibernating":false}]
+    """
 
-  #   assert remove_whitespace(response) == remove_whitespace(expected_response)
-  # end
+    assert remove_whitespace(response) == remove_whitespace(expected_response)
+  end
 
   # test "POST /api/bears" do
   #   request = """
